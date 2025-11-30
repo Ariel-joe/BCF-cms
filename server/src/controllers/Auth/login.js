546e1f242c1c14dca7c08e1ae40ca410fffbb36d
Ariel-joe <arieljoe00@gmail.com>
2025-11-30 11:00:27 +0300

    cookie

--- server/src/controllers/Auth/login.js
+++ server/src/controllers/Auth/login.js
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
             path: "/",
         };
 
