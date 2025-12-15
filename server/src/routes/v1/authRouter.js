290d340ed9eb4dc54f0438ef827c00aefa741366
Ariel-joe <arieljoe00@gmail.com>
2025-12-15 22:30:54 +0300

    update on minor permissions

--- server/src/routes/v1/authRouter.js
+++ server/src/routes/v1/authRouter.js
@@ -4,13 +4,25 @@ import { login } from "../../controllers/Auth/login.js";
 import { forgotPassword } from "../../controllers/Auth/forgot-pass.js";
 import { resetPassword } from "../../controllers/Auth/reset-Pass.js";
 import { logout } from "../../controllers/Auth/logout.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
+import { permissionLoader } from "../../middleware/permissionMiddleware.js";
+import { checkPermission } from "../../middleware/rbacMiddleware.js";
 
 const authRouter = Router();
 
-authRouter.post("/auth/signup", Signup);
+// Only users with "user:create" can make a signup (create account)
+authRouter.post(
+    "/auth/signup",
+    authMiddleware,
+    permissionLoader,
+    checkPermission("user:create"),
+    Signup
+);
+
+// Public routes
 authRouter.post("/auth/login", login);
-authRouter.post("/auth/logout", logout)
-authRouter.post("/auth/forgot-pass", forgotPassword);
-authRouter.post("/auth/reset-pass/:token", resetPassword)
+authRouter.post("/auth/logout", logout);
+authRouter.post("/auth/forgot-pass", authMiddleware, permissionLoader, checkPermission("user:edit"), forgotPassword);
+authRouter.post("/auth/reset-pass/:token", resetPassword);
 
 export { authRouter };
