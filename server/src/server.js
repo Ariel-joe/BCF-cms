4d8721fa811fa03929eef6f153812c857868cb8b
Ariel-joe <arieljoe00@gmail.com>
2025-12-16 10:31:12 +0300

    remove passport

--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,6 @@ import "dotenv/config";
 import { connectDb } from "./database/config.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import compression from "compression";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-// import passport from "./controllers/Auth/passport-google.js";
 import { authRouter } from "./routes/v1/authRouter.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
 import { blogRouter } from "./routes/v1/blogRouter.js";
@@ -21,8 +17,7 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-// TODO: uncompress the server later
-// app.use(compression())
+
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
@@ -43,26 +38,7 @@ app.use(express.json());
 // Parse cookies
 app.use(cookieParser());
 
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGO_URI,
-            collectionName: "sessions",
-            ttl: 24 * 60 * 60, // 1 day
-        }),
-        cookie: {
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        },
-    })
-);
 
-// // Initialize passport
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 // public routers
 app.use("/api/v1", donationRouter);
@@ -80,19 +56,6 @@ app.use(
     roleRouter
 );
 
-// // Passport Google OAuth routes
-// app.get(
-//     "/auth/google",
-//     passport.authenticate("google", { scope: ["profile", "email"] })
-// );
-
-// app.get(
-//     "/auth/google/callback",
-//     passport.authenticate("google", {
-//         failureRedirect: "/auth/signin?error=oauth",
-//         successRedirect: "/",
-//     })
-// );
 
 app.get("/", (req, res) => {
     res.json({
